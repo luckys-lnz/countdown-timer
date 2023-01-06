718076fe173fbb80decc74d86ebd4463ecfae5d6
LuckyArchibong <lalucky22@gmail.com>
2023-01-06 10:33:57 +0000

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Timer from './components/Timer'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
@@ -11,6 +11,36 @@ function App() {
   const [timerMins, setTimerMins] = useState();
 
   const [timerSecs, setTimerSecs] = useState();
+
+  let interval;
+
+  const startTimer = () => {
+    const countDownDate = new Date("May 30,2021 ").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+      const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+      const seconds = Math.floor((distance % (60 * 1000)) / (1000));
+      
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHrs(hours);
+        setTimerMins(minutes);
+        setTimerSecs(seconds);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  })
 
   return (
 
